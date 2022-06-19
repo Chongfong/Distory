66318d0844a182c9b3b1fd6416125214a445415d
Chongfong <wl00796865@gmail.com>
2022-06-19 17:29:14 +0800

    feat: image-editor with URL upload and get URL feature

--- src/components/ImageEditor.js
+++ src/components/ImageEditor.js
@@ -1,30 +1,60 @@
 import 'tui-image-editor/dist/tui-image-editor.css';
 import ImageEditor from '@toast-ui/react-image-editor';
-import React from 'react';
+import React, { useRef } from 'react';
+import LoadFromUrl from './LoadFromURL';
+import myTheme from './imageEditorTheme';
 
 export default function PhotoEditor() {
+  const editorRef = useRef();
+  const loadFromUrl = (url) => {
+    const editorInstance = editorRef.current.getInstance();
+    editorInstance.loadImageFromURL(url, 'image');
+  };
+
+  const logImageContent = () => {
+    const editorInstance = editorRef.current.getInstance();
+    const dataURL = editorInstance.toDataURL();
+    console.log(dataURL);
+  };
+
   return (
-    <ImageEditor
-      includeUI={{
-        loadImage: {
-          path: 'img/sampleImage.jpg',
-          name: 'SampleImage',
-        },
-        menu: ['shape', 'filter'],
-        initMenu: 'filter',
-        uiSize: {
-          width: '1000px',
-          height: '700px',
-        },
-        menuBarPosition: 'bottom',
-      }}
-      cssMaxHeight={500}
-      cssMaxWidth={700}
-      selectionStyle={{
-        cornerSize: 20,
-        rotatingPointOffset: 70,
-      }}
-      usageStatistics
-    />
+    <>
+      <LoadFromUrl onUrlLoadClick={(url) => loadFromUrl(url)} />
+      <ImageEditor
+        ref={editorRef}
+        includeUI={{
+          loadImage: {
+            path: 'https://i1.wp.com/www.sozailab.jp/db_img/sozai/49644/5d3e6234c3eab5203aad1df111f90f5d.png',
+            name: 'SampleImage',
+          },
+          theme: myTheme,
+          menu: [
+            'crop',
+            'flip',
+            'rotate',
+            'draw',
+            'shape',
+            'icon',
+            'text',
+            'mask',
+            'filter',
+          ],
+          initMenu: 'filter',
+          uiSize: {
+            width: '1000px',
+            height: '700px',
+          },
+          menuBarPosition: 'bottom',
+          cssMaxWidth: 700,
+          cssMaxHeight: 500,
+          selectionStyle: {
+            cornerSize: 20,
+            rotatingPointOffset: 70,
+          },
+          usageStatistics: false,
+        }}
+      />
+      <button type="button" onClick={logImageContent}>Get URL </button>
+    </>
   );
 }
