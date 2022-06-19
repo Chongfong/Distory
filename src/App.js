fa7f1f7e7db1f22b778d193fdc04d9e1c748645a
Chongfong <wl00796865@gmail.com>
2022-06-19 22:12:30 +0800

    feat: integrate image editor with text editor -show edited image to text editor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import CreateNewDiary from './pages/CreateNewDiary';
-import PhotoEditor from './components/ImageEditor';
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <header className="App-header">
         <h1>發布新文章</h1>
         <CreateNewDiary />
-        <PhotoEditor />
       </header>
     </div>
   );
--- src/components/ImageEditor.js
+++ src/components/ImageEditor.js
@@ -1,11 +1,13 @@
 import 'tui-image-editor/dist/tui-image-editor.css';
 import ImageEditor from '@toast-ui/react-image-editor';
 import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
 import LoadFromUrl from './LoadFromURL';
 import myTheme from './imageEditorTheme';
 
-export default function PhotoEditor() {
+export default function PhotoEditor({ setImageUrl, diaryContentValue, setDiaryContentValue }) {
   const editorRef = useRef();
+
   const loadFromUrl = (url) => {
     const editorInstance = editorRef.current.getInstance();
     editorInstance.loadImageFromURL(url, 'image');
@@ -14,7 +16,8 @@ export default function PhotoEditor() {
   const logImageContent = () => {
     const editorInstance = editorRef.current.getInstance();
     const dataURL = editorInstance.toDataURL();
-    console.log(dataURL);
+    setImageUrl(dataURL);
+    setDiaryContentValue(`${diaryContentValue} <img src="${dataURL}">`);
   };
 
   return (
@@ -27,6 +30,23 @@ export default function PhotoEditor() {
             path: 'https://i1.wp.com/www.sozailab.jp/db_img/sozai/49644/5d3e6234c3eab5203aad1df111f90f5d.png',
             name: 'SampleImage',
           },
+          // locale: {
+          //   DeleteAll: '全部刪除',
+          //   Delete: '刪除',
+          //   Text: '文字',
+          //   Shape: '形狀',
+          //   Load: '讀取檔案',
+          //   Download: '下載檔案',
+          //   Crop: '裁切',
+          //   Undo: '上一步',
+          //   Redo: '下一步',
+          //   'Text size': '字體大小',
+          //   Color: '顏色',
+          //   Cancel: '取消',
+          //   Rectangle: '矩形',
+          //   Triangle: '三角形',
+          //   Circle: '圓形',
+          // },
           theme: myTheme,
           menu: [
             'crop',
@@ -58,3 +78,15 @@ export default function PhotoEditor() {
     </>
   );
 }
+
+PhotoEditor.propTypes = {
+  diaryContentValue: PropTypes.string,
+  setImageUrl: PropTypes.func,
+  setDiaryContentValue: PropTypes.func,
+};
+
+PhotoEditor.defaultProps = {
+  diaryContentValue: '',
+  setImageUrl: () => {},
+  setDiaryContentValue: () => {},
+};
